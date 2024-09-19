1ee62550b4779bc309d60755fa0ac6175c57e751
Thiago-S-O <thiagosoxiii@outlook.com>
2024-09-19 19:58:00 -0300

    subida projeto: sorteio de numeros

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sorteador-numeros
 projeto sorteador de números, praticar lógica de programação
+
+## criação da logica de sorteio;
+- utilização do `for` para popular um array com os numeros de acordo com o intervalo dado pelo usuário;
+- lógica para gerar os números de forma aleatória da lista gerada pelo `for` loop;
+- verficação para não retornar números repetidos na lista a ser retornada ao usuário;
+- lógica para gerar o array com os valores sorteados para o usuário;
+- lógica para quando o usuário não preencher os campos adequadamente.
--- app.js
+++ app.js
@@ -5,35 +5,81 @@ let numberList = [];
 let resultList = [];
 
 function intervalChosenOfNumberList(minNumber, maxNumber) {
-  for (let i = 0; i < maxNumber - minNumber + 1; i++) {
-    numberList.push(minNumber + i);
+  for (let i = 0; i < (maxNumber - minNumber); i++) {
+    // se deixar só minNumber + i, o js concatena já que é a soam de variáveis do tipo any
+    let inputNumberInArray = minNumber - 1 + i + 1;
+    numberList.push(inputNumberInArray);
   }
-  // console.log(numberList);
   return numberList;
 }
-// console.log(intervalChosenOfNumberList(10, 20));
+
+let list = intervalChosenOfNumberList(minNumber, maxNumber);
 
 function chosenRandowNumber() {
-  const list = intervalChosenOfNumberList(10, 20);
-  console.log('@lista com os números em sequencia:', list);
-
   let randowIndex = Math.floor(Math.random() * list.length);
   let randowNumber = list[randowIndex];
-  console.log(randowNumber);
   return randowNumber;
 }
 
-// console.log(chosenRandowNumber());
-function verifyArrayRandowNumber(quantitieOfNumber) { // refazer esta parte da lógica
-  while (resultList.length < quantitieOfNumber) {
-    let exists = resultList.includes(chosenRandowNumber());
-    if (exists) {
-      verifyArrayRandowNumber();
-    } else {
-      resultList.push(chosenRandowNumber());
-    }
+function verifyArrayRandowNumber() {
+  let drawnNumber = chosenRandowNumber();
+
+  if (!resultList.includes(drawnNumber)) {
+    resultList.push(drawnNumber);
   }
   return resultList;
 }
 
-console.log(verifyArrayRandowNumber(3));
+function generateArray(quantitieOfNumber) {
+  while (resultList.length < quantitieOfNumber) {
+    verifyArrayRandowNumber();
+  }
+  return resultList;
+}
+
+// parte da interação com o html 
+function showTextInScreen(id, textShow) {
+  let title = document.getElementById(id);
+  title.innerHTML = textShow;
+  // responsiveVoice.speak(textShow, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function clearInput(id) {
+  let field = document.getElementById(id);
+  field.value = "";
+}
+
+function restartGame() {
+  numberList = [];
+  resultList = [];
+  clearInput('de');
+  clearInput('ate');
+  clearInput('quantidade');
+  showTextInScreen('resultado-tela',`Números sorteados: nenhum até agora`);
+  document.getElementById("btn-reiniciar").setAttribute("disabled", true);
+  document.getElementById("btn-sortear").removeAttribute("disabled");
+}
+
+function drawnNumbers() { // sortear
+  minNumber = document.getElementById('de').value;
+  maxNumber = document.getElementById('ate').value;
+  quantitieOfNumber = document.getElementById('quantidade').value;
+
+  if ((minNumber & maxNumber & quantitieOfNumber == 0) || (maxNumber <= minNumber) || (quantitieOfNumber > (maxNumber - minNumber))) {
+    showTextInScreen('resultado-tela',`
+      Parâmetros inválidos!
+      Atenção a algumas regras:
+        - Preencher todos os campos;
+        - A quantidade não pode ser maior do que a o intervalos dos valores;
+        - O valor máximo não pode ser igual ou menor do que o valor mínimo.
+    `);
+  } else {
+    intervalChosenOfNumberList(minNumber, maxNumber);
+    let result = generateArray(quantitieOfNumber);
+    showTextInScreen('resultado-tela',`Números sorteados: ${result}`);
+    console.log(result);
+  }
+
+  document.getElementById("btn-reiniciar").removeAttribute("disabled");
+  document.getElementById("btn-sortear").setAttribute("disabled", true);
+}
